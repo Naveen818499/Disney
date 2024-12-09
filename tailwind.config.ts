9a2c4f9fcab058d3511fb365ba5e355cb45bd0f1
Nanisushu@2117 <naveenthanniru2107@gmail.com>
2024-12-09 15:40:13 +0530

    names changed

---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('tailwind-scrollbar-hide')
+//   ],
+// }
+
+/** @type {import('tailwindcss').Config} */
+import scrollbarHide from 'tailwind-scrollbar-hide';
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    scrollbarHide,
+  ],
+}
